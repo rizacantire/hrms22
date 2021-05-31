052c986c3b878620a7aad8b62adf81ad1b9c8aa6
rza35 <riza_tire@hotmail.com>
2021-06-01 01:03:07 +0300

    31.05

--- src/main/java/com/kodlamaio/hrms/core/entities/User.java
+++ src/main/java/com/kodlamaio/hrms/core/entities/User.java
@@ -3,18 +3,21 @@ package com.kodlamaio.hrms.core.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.UniqueElements;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import java.lang.annotation.Repeatable;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "users")
 @Entity
+
 public class User {
 
     @Id
@@ -29,9 +32,11 @@ public class User {
     @Column(name="mail")
     private String mail;
 
+
     @NotEmpty
     @NotBlank
     @NotNull
     @Column(name="password")
+
     private String password;
 }
